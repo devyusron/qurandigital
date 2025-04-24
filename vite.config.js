1f9dbbff9cc8ae0b806b342fa16c4bdb4cef6190
my <muhammad.yusron@jadinpratama.com>
2025-04-24 21:45:10 +0700

    add dist

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/qurandigital/',
   plugins: [
-    tailwindcss(),
     svelte()
   ],
 })
